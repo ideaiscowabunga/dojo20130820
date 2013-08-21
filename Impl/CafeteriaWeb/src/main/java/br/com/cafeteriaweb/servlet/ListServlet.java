677126ccd4802f3d44d93a7d324918381d8bb91a
Juliano Fernandes <juliano.hs.fernandes@gmail.com>
2013-08-21 00:36:06 -0300

    Corrigindo teste bug do bean do spring no servlet

--- Impl/CafeteriaWeb/src/main/java/br/com/cafeteriaweb/servlet/ListServlet.java
+++ Impl/CafeteriaWeb/src/main/java/br/com/cafeteriaweb/servlet/ListServlet.java
@@ -4,38 +4,23 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.springframework.web.HttpRequestHandler;
 
 import br.com.cafeteriaweb.dao.CafeDao;
 
-public class ListServlet extends HttpServlet {
+public class ListServlet implements HttpRequestHandler {
 	
-	private static final long serialVersionUID = 1L;
-
 	@Autowired
 	private CafeDao cafeDao;
-	
-    public ListServlet() {
-        super();
-        
-    }
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@Override
+	public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("cafes", cafeDao.findAll() );
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("list.jsp");
 		requestDispatcher.forward(request, response);
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
-
-	public void setCafeDao(CafeDao cafeDao) {
-		this.cafeDao = cafeDao;
 	}
 }
